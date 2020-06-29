a9ab6ef9df8feefc27b5625c8de23481734579c5
leezhenwang <1738789165@qq.com>
2020-06-29 20:56:54 +0800

    feat:增加z

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,20 @@ import './index.css'
 import './index.scss'
 import {Button} from 'antd'
 import Header from '../component/header/header'
+import { Fragment } from 'react'
 export default function Home() {
   return (
-    <>
+    <Fragment>
       <Head>
         <title>Home</title>
       </Head>
       <Header />
       {/* <div className="home">首页</div>
       <Button>这是首页button</Button> */}
-
-    </>
+      <div className="blog-container">
+        <div className="blog-left"></div>
+        <div className="blog-right"></div>
+      </div>
+    </Fragment>
   )
 }
