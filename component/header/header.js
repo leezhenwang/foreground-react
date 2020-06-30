05dfe1b6278e839b0d846a34f2be89e86fbd94a3
LAPTOP-GHR86GEE\17387 <1738789165@qq.com>
2020-06-30 08:52:54 +0800

    fix: 修改头部文本

--- component/header/header.js
+++ component/header/header.js
@@ -5,8 +5,8 @@ const Header = () => (
   <div className="header-contianer">
     <div className="header-box">
       <div className="header-left">xxx工作室<span className="studio-descr">专注前端开发</span></div>
-      <div className="hearder-icon"><HomeOutlined />&nbsp;<span className="icon-text">首页</span></div>
-      <div className="hearder-icon"><FundProjectionScreenOutlined />&nbsp;<span className="icon-text">生活</span></div>
+      <div className="hearder-icon"><HomeOutlined />&nbsp;<span className="icon-text">博客首页</span></div>
+      <div className="hearder-icon"><FundProjectionScreenOutlined />&nbsp;<span className="icon-text">记录生活</span></div>
     </div>
   </div>
 )
