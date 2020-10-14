e5ea7dac7aaf242853061ee43614e0b50e6c8049
LAPTOP-GHR86GEE\17387 <1738789165@qq.com>
2020-10-14 09:45:38 +0800

    fix: 修复接口为域名

--- config/apiUrl.js
+++ config/apiUrl.js
@@ -1,7 +1,7 @@
 let isOnline = true
-let ipUrl = isOnline? 'http://129.204.206.80:7001' : 'http://127.0.0.1:7001' //本地
+let ipUrl = isOnline? 'http://leezhenwang.website' : 'http://127.0.0.1:7001' //本地 线上ip：http://129.204.206.80:7001
 // let ipUrl = 'http://129.204.206.80:7001' //线上
-let url = isOnline ? 'http://129.204.206.80:7001' : 'http://127.0.0.1:7001'
+let url = isOnline ? 'http://leezhenwang.website' : 'http://127.0.0.1:7001' // 线上ip：http://129.204.206.80:7001
 let servicePath = {
   getArticleList: ipUrl + '/default/getArticleList' ,  //  首页文章列表接口
   getArticleById: url + '/default/getArticleById',  // 文章详细页内容接口 ,需要接收参数
