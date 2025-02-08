ba62c48986f1df480fa8ed71c1188a2acd192799
leezhenwang <1738789165@qq.com>
2025-02-08 10:40:27 +0800

    feat: 增加备案号

--- component/footer/footer.js
+++ component/footer/footer.js
@@ -2,6 +2,7 @@ import './footer.scss'
 const Footer =()=>{
   return <div className="footer-div">
   <div>系统由 React+Node+Ant Desgin驱动 </div>
+  <div>ICP备案/许可证号：粤B2 </div>
 </div>
 }
 export default Footer;
